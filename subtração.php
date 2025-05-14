af0c221687f8654183cd1c0107559c72ffd9aec6
gns777 <gustavonsoletti@yahoo.com>
2025-05-14 09:14:30 -0300

    Add files via upload

---
+++ subtração.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+echo "<h1> Calculadora</h1>";
+$a = 10;
+$b = 5;
+$soma= $a + $b;
+echo "<h2> soma</h2>";
+echo"$a + $b = $soma";
+echo "<h2> Subtração </h2>";
+$subtracao = $a - $b;
+echo "$a - $b = $subtracao";
+?> 
+
+</body>
+</html>
